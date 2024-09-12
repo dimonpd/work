8972fd36ca1ca3c01a1346599a3cb1df167a9df7
dimonpd <84992849+dimonpd@users.noreply.github.com>
2024-09-12 20:24:49 +0300

    1

---
+++ streamlit_app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import joblib
+
+
+rf = joblib.load('rf_model.joblib')
+# Создайте форму для ввода данных
+st.title('Прогнозирование')
+st.write('Введите данные для прогнозирования')
+
+# Создайте поля для ввода данных
+X_input = st.number_input('X', min_value=0, max_value=100)
+
+# Создайте кнопку для запуска прогнозирования
+if st.button('Прогнозировать'):
+    # Прогнозируйте значение
+    y_pred = rf.predict([[X_input]])
+    st.write(f'Прогнозируемое значение: {y_pred[0]}')
